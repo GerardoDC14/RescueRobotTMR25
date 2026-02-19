0b5f0266941a6e2eaf5ea160923a7ff8ba3c20e4
Gerardo Escobar Del Cid <123440177+GerardoDC14@users.noreply.github.com>
2026-02-18 21:33:58 -0600

    Delete jaguar_odom/build directory

--- jaguar_odom/build/astra_camera/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/gerardo/Projects/Robotics/RescueRobot2025/RescueRobotTMR25/jaguar_odom/install/astra_camera_msgs/share/astra_camera_msgs/package.sh"
--- jaguar_odom/build/jaguar_odom/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/gerardo/Projects/Robotics/RescueRobot2025/RescueRobotTMR25/jaguar_odom/install/jaguar_odom'
